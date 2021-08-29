b614b33f3baceb0ed1a271b8e14941b29868a144
Yahor Matsukov <52573595+eeggoor@users.noreply.github.com>
2021-08-29 13:02:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newStr = str;
+  for (let i = 0; i < bracketsConfig.length * 2; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      let subStr = bracketsConfig[j][0] + bracketsConfig[j][1];
+      for (let j = 0; j < str.length / 2; j++) {
+        newStr = newStr.replace(subStr, '');
+      }
+    }
+    if (newStr.length === 0) return true;
+  }
+  if (newStr.length > 0) return false;
 }
